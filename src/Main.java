b42eca16ec6f69e1f197b7e1617fa8a6e40fccd9
BIGJAY5000 <144457717+BIGJAY5000@users.noreply.github.com>
2024-01-04 09:42:46 -0500

    trh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] vals = {1 ,2 ,3 ,4 , 5};
+        shiftRight(vals);
+        for (int i = 0; i < vals.length; i ++) {
+            System.out.print(vals[i] + " ");
+        }
+    }
+    public static int [] insert(int [] arr, int element, int index){
+        int [] nums = arr;
+        for (int i = nums.length - 1;i > index; i --) {
+            nums[i] = nums[i-1];
+        }
+        nums[index] = element;
+        return nums;
+    }
+
+    public static int [] shiftRight(int [] nums){
+        int temp = nums[nums.length -1];
+        for (int i = nums.length - 1; i > 0; i--) {
+                nums[i] = nums[i - 1];
+        }
+        nums[0] = temp;
+        return nums;
     }
 }
